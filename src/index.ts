1cbb3eda7a900504003876e38b739a6db1b58e5a
EastOrb <102253515+EastOrb@users.noreply.github.com>
2023-08-05 13:58:14 +0100

    Update index.ts
    input validation for price and quantity, handling scenarios when the shoe with the provided id is not found, and also added checks for empty or null keywords in the search function. Additionally, I renamed the updateStore function to updateShoe to keep the naming consistent.

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ type Shoe = Record<{
     size: string;
     shoeURL: string;
     price: int16;
-    quantity: string
+    quantity: string;
     rating: float32;
     createdAt: nat64;
     updatedAt: Opt<nat64>;
@@ -19,14 +19,21 @@ type ShoePayload = Record<{
     shoeURL: string;
     price: int16;
     quantity: string;
-    // rating:float32;
 }>
 
 const shoeStorage = new StableBTreeMap<string, Shoe>(0, 44, 1024)
 
 $update;
 export function createShoe(payload: ShoePayload): Result<Shoe, string> {
-    const shoe: Shoe = { id: uuidv4(), createdAt: ic.time(),rating: 1.0,updatedAt: Opt.None, ...payload };
+    // Input validation for price and quantity
+    if (payload.price <= 0) {
+        return Result.Err<Shoe, string>('Price must be greater than 0');
+    }
+    if (payload.quantity.trim() === '') {
+        return Result.Err<Shoe, string>('Quantity cannot be empty');
+    }
+
+    const shoe: Shoe = { id: uuidv4(), createdAt: ic.time(), rating: 1.0, updatedAt: Opt.None, ...payload };
     shoeStorage.insert(shoe.id, shoe);
     return Result.Ok(shoe);
 }
@@ -37,101 +44,95 @@ export function getShoe(): Vec<Shoe> {
     return shoeStorage.values();
 }
 
-//gets a particular shoe using the shoe's id
-$query;
+// gets a particular shoe using the shoe's id
+$query
 export function getShoeById(id: string): Result<Shoe, string> {
     return match(shoeStorage.get(id), {
         Some: (shoe) => Result.Ok<Shoe, string>(shoe),
-        //throws an Error when a shoe with the id is not found
-        None: () => Result.Err<Shoe, string>(`a shoe with id=${id} not found`)
+        None: () => Result.Err<Shoe, string>(`A shoe with id=${id} not found`),
     });
 }
 
-//function that search for a shoe product
-$query;
+// function that searches for a shoe product
+$query
 export function searchShoeProduct(keyword: string): Result<Vec<Shoe>, string> {
-    const result = shoeStorage.values().filter((shoe) => {
-//variable that uses the "name" keyword to search for a particular shoes      
-        const value = shoe.name.includes(keyword)
-        return value;
-    });
+    if (!keyword || keyword.trim() === '') {
+        return Result.Err<Vec<Shoe>, string>('Keyword cannot be empty');
+    }
+    const result = shoeStorage.values().filter((shoe) => shoe.name.includes(keyword));
     return Result.Ok<Vec<Shoe>, string>(result);
 }
 
-
-
-
 // Function for rating a shoe
-$update;
+$update
 export function rateShoe(id: string, rate: number): Result<Shoe, string> {
-    
-    //make sure the ratin rage is not less than 0 or greater than 4
+    // Make sure the rating range is not less than 0 or greater than 4
     if (rate < 0 || rate > 4) {
-        return Result.Err<Shoe, string>(
-          `Error rating shoe with the id=${id}. Invalid rating value. Value should not be more than 4 or less than 0`
-        );
-      }
-    
-    // Gets the shoe details by it's id
-    const shoeRating: any = match(shoeStorage.get(id), {
-    
-    // returns the current rating value 
-        Some: (shoe) => {
-            return shoe.rating;
-        },
-        None: () => Result.Err<Shoe, string>(`Error updating shoe with the id=${id}. shoe not found`)
-    })
+        return Result.Err<Shoe, string>('Error rating shoe. Invalid rating value. Value should not be more than 4 or less than 0');
+    }
 
-    // Calculates the new rating by adding the current rating to the user's 
+    // Gets the shoe details by its id
+    const shoe = match(shoeStorage.get(id), {
+        Some: (shoe) => shoe,
+        None: () => return Result.Err<Shoe, string>(`Error rating shoe with id=${id}. Shoe not found`),
+    });
+
+    // Calculates the new rating by adding the current rating to the user's
     // rating and dividing the result by 4
-    const rating: any = ((shoeRating + rate) / 4);
+    const newRating: float32 = (shoe.rating + rate) / 4;
 
-    return match(shoeStorage.get(id), {
-        Some: (result) => {
-            const shoe: Shoe = {
-                ...result,
-                rating,
-            };
-            shoeStorage.insert(shoe.id, shoe);
-            return Result.Ok<Shoe, string>(shoe);
-        },
-        None: () => Result.Err<Shoe, string>(`Error rating shoe with the id=${id}. shoe not found`)
-    });
+    const updatedShoe: Shoe = {
+        ...shoe,
+        rating: newRating,
+    };
+
+    shoeStorage.insert(updatedShoe.id, updatedShoe);
+    return Result.Ok<Shoe, string>(updatedShoe);
 }
 
-//delete a specific show using the show id
-$update;
+// Delete a specific shoe using the shoe id
+$update
 export function deleteShoe(id: string): Result<Shoe, string> {
-    return match(shoeStorage.remove(id), {
-        Some: (deletedShoe) => Result.Ok<Shoe, string>(deletedShoe),
-        None: () => Result.Err<Shoe, string>(`couldn't delete a shoe with id=${id}. shoe not found.`)
+    const shoe = match(shoeStorage.remove(id), {
+        Some: (deletedShoe) => deletedShoe,
+        None: () => return Result.Err<Shoe, string>(`Couldn't delete shoe with id=${id}. Shoe not found.`),
     });
+    return Result.Ok<Shoe, string>(shoe);
 }
 
-
-
-$update;
-export function updateStore(id: string, payload: ShoePayload): Result<Shoe, string> {
-    return match(shoeStorage.get(id), {
-        Some: (shoe) => {
-            const updatedStore: Shoe = {...shoe, ...payload, updatedAt: Opt.Some(ic.time())};
-            shoeStorage.insert(shoe.id, updatedStore);
-            return Result.Ok<Shoe, string>(updatedStore);
-        },
-        None: () => Result.Err<Shoe, string>(`couldn't update a shoe with id=${id}. shoe not found`)
+// Update the details of a shoe in the store
+$update
+export function updateShoe(id: string, payload: ShoePayload): Result<Shoe, string> {
+    // Check if the shoe exists
+    const shoe = match(shoeStorage.get(id), {
+        Some: (shoe) => shoe,
+        None: () => return Result.Err<Shoe, string>(`Couldn't update shoe with id=${id}. Shoe not found`),
     });
+
+    // Input validation for price and quantity
+    if (payload.price <= 0) {
+        return Result.Err<Shoe, string>('Price must be greater than 0');
+    }
+    if (payload.quantity.trim() === '') {
+        return Result.Err<Shoe, string>('Quantity cannot be empty');
+    }
+
+    const updatedShoe: Shoe = {
+        ...shoe,
+        ...payload,
+        updatedAt: Opt.Some(ic.time()),
+    };
+    shoeStorage.insert(updatedShoe.id, updatedShoe);
+    return Result.Ok<Shoe, string>(updatedShoe);
 }
 
 // a workaround to make uuid package work with Azle
 globalThis.crypto = {
     getRandomValues: () => {
         let array = new Uint8Array(32);
-
         for (let i = 0; i < array.length; i++) {
             array[i] = Math.floor(Math.random() * 256);
         }
-
         return array;
-    }
+    },
 };
-
